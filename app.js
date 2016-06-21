75091f6b09e56be37b1c03cf0b08a0ec3be29952
Erica Winberry <elaewin@gmail.com>
2016-06-21 15:31:39 -0700

    Removed opening and closing hours from constructor and made a storeOpenHours array. Deleted the function with the same name. Removed all lists from html. added empty table tags to sales.html.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Array of stores
 var allStores = [];
 
+// Array of hours the stores are open
+var openHoursArray = ['6:00 - 7:00am', '7:00 - 8:00am', '8:00 - 9:00am', '9:00 - 10:00am', '10:00 - 11:00am', '11:00 - 12:00pm', '12:00 - 1:00pm', '1:00 - 2:00pm', '2:00 - 3:00pm', '3:00 - 4:00pm', '4:00 - 5:00pm', '5:00 - 6:00pm', '6:00 - 7:00pm', '7:00 - 8:00pm'];
+
 // Returns a random integer between min (included) and max (included)
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -12,32 +15,17 @@ function Store(identifier, storeName, storeAddress, storePhone, storeOpens, stor
   this.storeName = storeName;
   this.storeAddress = storeAddress;
   this.storePhone = storePhone;
-  this.storeOpens = storeOpens; // time the store opens, 24 hour clock
-  this.storeCloses = storeCloses; // time the store closes, 24 hour clock
   this.minCustsPerHour = minCustsPerHour;
   this.maxCustsPerHour = maxCustsPerHour;
   this.avgCookiesPerCust = avgCookiesPerCust;
-  this.openHoursArray = [];
   this.custsPerHourArray = [];
   this.cookiesPerHourArray = [];
   this.dailySalesTotal = 0;
-
   allStores.push(this);
-
-  // Generate and array of the hours the store is open, in a string
-  this.createOpenHours = function() {
-    var storeIsOpen = this.storeCloses - this.storeOpens;
-    for (var i = 0; i < storeIsOpen; i++) {
-      var timeAsString = (i + this.storeOpens) + ':00 - ' + (i + this.storeOpens + 1) + ':00';
-      // console.log('Time:', timeAsString);
-      this.openHoursArray.push(timeAsString);
-    }
-  };
 
   // Updates a store's array of custsPerHourArray with a random number of customers per hour for each hour that the store is open.
   this.generateHourlyTraffic = function() {
-    hoursOfOperation = this.createOpenHours();
-    for (var i = 0; i < this.openHoursArray.length; i++) {
+    for (var i = 0; i < openHoursArray.length; i++) {
       var customers = getRandomIntInclusive(this.minCustsPerHour, this.maxCustsPerHour);
       // console.log('Customers per hour:', customers);
       this.custsPerHourArray.push(customers);
@@ -49,7 +37,7 @@ function Store(identifier, storeName, storeAddress, storePhone, storeOpens, stor
     this.generateHourlyTraffic();
     var hourlyCookieAverage = this.avgCookiesPerCust;
     var totalCookies = 0;
-    for (var i = 0; i < this.openHoursArray.length; i++) {
+    for (var i = 0; i < openHoursArray.length; i++) {
       var cookies = Math.floor(this.custsPerHourArray[i] * this.avgCookiesPerCust);
       // console.log('Cookies this hour:', cookies);
       totalCookies += cookies;
@@ -65,7 +53,7 @@ function Store(identifier, storeName, storeAddress, storePhone, storeOpens, stor
     var salesList = document.getElementById(this.identifier);
     for (var i = 0; i < this.cookiesPerHourArray.length; i++) {
       var listItem = document.createElement('li');
-      listItem.textContent = this.openHoursArray[i] + ': ' + this.cookiesPerHourArray[i] + ' cookies';
+      listItem.textContent = openHoursArray[i] + ': ' + this.cookiesPerHourArray[i] + ' cookies';
       salesList.appendChild(listItem);
     }
     var listItem = document.createElement('li');
@@ -74,22 +62,29 @@ function Store(identifier, storeName, storeAddress, storePhone, storeOpens, stor
   };
 };
 
-var firstAndPike = new Store('firstandpike', '1st and Pike', '102 Pike St, Seattle, WA 98101', '206-xxx-xxxx', 6, 20, 23, 65, 6.3);
+var firstAndPike = new Store('firstandpike', '1st and Pike', '102 Pike St, Seattle, WA 98101', '206-xxx-xxxx', 23, 65, 6.3);
 
-var seatac = new Store('seatac', 'Seatac Airport', 'Concourse D, 17801 International Blvd, Seattle, WA 98158', '425-xxx-xxxx', 6, 20, 3, 24, 1.2);
+var seatac = new Store('seatac', 'Seatac Airport', 'Concourse D, 17801 International Blvd, Seattle, WA 98158', '425-xxx-xxxx', 3, 24, 1.2);
 
-var seattleCenter = new Store('seattlecenter', 'Seattle Center', '305 Harrison St, Seattle, WA 98109', '206-xxx-xxxx', 6, 20, 11, 38, 3.7);
+var seattleCenter = new Store('seattlecenter', 'Seattle Center', '305 Harrison St, Seattle, WA 98109', '206-xxx-xxxx', 11, 38, 3.7);
 
-var capitolHill = new Store('capitolhill', 'Capitol Hill', '434 Broadway Avenue E, Seattle, WA 98102', '206-xxx-xxxx', 6, 20, 20, 38, 2.3);
+var capitolHill = new Store('capitolhill', 'Capitol Hill', '434 Broadway Avenue E, Seattle, WA 98102', '206-xxx-xxxx', 20, 38, 2.3);
 
-var alki = new Store('alki', 'Alki', '2742 Alki Ave SW; Seattle, WA 98116', '206-xxx-xxxx', 6, 20, 2, 16, 4.6);
+var alki = new Store('alki', 'Alki', '2742 Alki Ave SW; Seattle, WA 98116', '206-xxx-xxxx', 2, 16, 4.6);
 
-// Generate sales numbers for each store in an array of stores
-function generateSalesNumbers() {
-  for (var i = 0; i < allStores.length; i++) {
-    console.dir(allStores[i]);
-    allStores[i].render();
+// Generate header row
+function makeHeaderRow() {
+  for(i = 0; i < openHoursArray.length; i++) {
+    
   }
 }
 
-generateSalesNumbers();
+// Generate sales numbers for each store in an array of stores
+// function generateSalesNumbers() {
+//   for (var i = 0; i < allStores.length; i++) {
+//     console.dir(allStores[i]);
+//     allStores[i].render();
+//   }
+// }
+//
+// generateSalesNumbers();
