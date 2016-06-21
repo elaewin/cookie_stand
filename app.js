53bb0fb101982a71ee11a781896624a5ed64c1c8
Erica Winberry <elaewin@gmail.com>
2016-06-21 12:58:01 -0700

    Madechanges suggested by today's lecture: made variable names more easily understandable, made each function call within the subsequent function, moved function to get store hours the first function that runs and used the results from that for the length of the following functions.

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 // MAYBE CLEAR THE ARRAYS IN EACH STORE BEFORE RUNNING THE FUNCTIONS TO FILL THEM.
 
 var firstAndPike = {
-  storeName: 'firstAndPike',
-  storeNamePretty: '1st and Pike',
+  identifier: 'firstAndPike',
+  storeName: '1st and Pike',
   storeAddress: '102 Pike St, Seattle, WA 98101',
   storePhone: '206-xxx-xxxx',
   storeOpens: 6, // time the store opens, 24 hour clock
   storeCloses: 20, // time the store closes, 24 hour clock
   openHoursArray: [],
-  minHourlyCustomers: 23,
-  maxHourlyCustomers: 65,
+  minCustsPerHour: 23,
+  maxCustsPerHour: 65,
   avgCookiesPerCust: 6.3,
   custsPerHourArray: [],
   cookiesPerHourArray: [],
@@ -19,15 +19,15 @@ var firstAndPike = {
 };
 
 var seatac = {
-  storeName: 'seatac',
-  storeNamePretty: 'Seatac Airport',
+  identifier: 'seatac',
+  storeName: 'Seatac Airport',
   storeAddress: 'Concourse D, 17801 International Blvd, Seattle, WA 98158',
   storePhone: '425-xxx-xxxx',
   storeOpens: 6, // time the store opens, 24 hour clock
   storeCloses: 20, // time the store closes, 24 hour clock
   openHoursArray: [],
-  minHourlyCustomers: 3,
-  maxHourlyCustomers: 24,
+  minCustsPerHour: 3,
+  maxCustsPerHour: 24,
   avgCookiesPerCust: 1.2,
   custsPerHourArray: [],
   cookiesPerHourArray: [],
@@ -35,15 +35,15 @@ var seatac = {
 };
 
 var seattleCenter = {
-  storeName: 'seattleCenter',
-  storeNamePretty: 'Seattle Center',
+  identifier: 'seattleCenter',
+  storeName: 'Seattle Center',
   storeAddress: '305 Harrison St, Seattle, WA 98109',
   storePhone: '206-xxx-xxxx',
   storeOpens: 6, // time the store opens, 24 hour clock
   storeCloses: 20, // time the store closes, 24 hour clock
   openHoursArray: [],
-  minHourlyCustomers: 11,
-  maxHourlyCustomers: 38,
+  minCustsPerHour: 11,
+  maxCustsPerHour: 38,
   avgCookiesPerCust: 3.7,
   custsPerHourArray: [],
   cookiesPerHourArray: [],
@@ -51,15 +51,15 @@ var seattleCenter = {
 };
 
 var capitolHill = {
-  storeName: 'capitolHill',
-  storeNamePretty: 'Capitol Hill',
+  identifier: 'capitolHill',
+  storeName: 'Capitol Hill',
   storeAddress: '434 Broadway Avenue E, Seattle, WA 98102',
   storePhone: '206-xxx-xxxx',
   storeOpens: 6, // time the store opens, 24 hour clock
   storeCloses: 20, // time the store closes, 24 hour clock
   openHoursArray: [],
-  minHourlyCustomers: 20,
-  maxHourlyCustomers: 38,
+  minCustsPerHour: 20,
+  maxCustsPerHour: 38,
   avgCookiesPerCust: 2.3,
   custsPerHourArray: [],
   cookiesPerHourArray: [],
@@ -67,15 +67,15 @@ var capitolHill = {
 };
 
 var alki = {
-  storeName: 'alki',
-  storeNamePretty: 'Alki',
+  identifier: 'alki',
+  storeName: 'Alki',
   storeAddress: '2742 Alki Ave SW; Seattle, WA 98116',
   storePhone: '206-xxx-xxxx',
   storeOpens: 6, // time the store opens, 24 hour clock
   storeCloses: 20, // time the store closes, 24 hour clock
   openHoursArray: [],
-  minHourlyCustomers: 2,
-  maxHourlyCustomers: 16,
+  minCustsPerHour: 2,
+  maxCustsPerHour: 16,
   avgCookiesPerCust: 4.6,
   custsPerHourArray: [],
   cookiesPerHourArray: [],
@@ -85,6 +85,19 @@ var alki = {
 // Array of stores
 var allStores = [firstAndPike, seatac, seattleCenter, capitolHill, alki];
 
+
+// Generate and array of the hours the store is open, in a string
+// NOTES FOR TUESDAY: MAKE THIS THE FIRST FUNCTION AND THEN USE THE LENGTH OF THIS ARRAY TO RUN THE FOR LOOP FOR THE LATER CALCULATIONS.
+function createOpenHours(store) {
+  var storeIsOpen = store.storeCloses - store.storeOpens;
+
+  for (var i = 0; i < storeIsOpen; i++) {
+    var timeAsString = (i + store.storeOpens) + ':00 - ' + (i + store.storeOpens + 1) + ':00';
+    // console.log('Time:', timeAsString);
+    store.openHoursArray.push(timeAsString);
+  }
+}
+
 // Returns a random integer between min (included) and max (included)
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -92,51 +105,36 @@ function getRandomIntInclusive(min, max) {
 
 // Updates a store's array of custsPerHourArray with a random number of customers per hour for each hour that the store is open.
 function generateHourlyTraffic(store) {
-  var storeIsOpen = store.storeCloses - store.storeOpens;
-  for (var i = 0; i < storeIsOpen; i++) {
-    var customers = getRandomIntInclusive(store.minHourlyCustomers, store.maxHourlyCustomers);
-    console.log('hourly customers at ' + (i + 6) + ' to ' + (i + 7) + ': ' + customers);
+  hoursOfOperation = createOpenHours(store);
+
+  for (var i = 0; i < store.openHoursArray.length; i++) {
+    var customers = getRandomIntInclusive(store.minCustsPerHour, store.maxCustsPerHour);
+    // console.log('Customers per hour:', customers);
     store.custsPerHourArray.push(customers);
   }
 }
 
 // Updates a store's array of cookiesPerHourArray by multiplying each item in a store's custsPerHourArray array by the average number of cookies sold per customer for that store.
-function projectedHourlySales(store) {
+function projectedSales(store) {
+  generateHourlyTraffic(store);
   var hourlyCookieAverage = store.avgCookiesPerCust;
-  for (var i = 0; i < store.custsPerHourArray.length; i++) {
-    var cookies = Math.floor(store.custsPerHourArray[i] * store.avgCookiesPerCust);
-    console.log('Cookies projected at ' + (i + 6) + ' to ' + (i + 7) + ': ' + cookies);
-    store.cookiesPerHourArray.push(cookies);
-  }
-}
-
-// Calculates the total number of cookies sold in a store based on the amounts in that store's cookiesPerHourArray array, returns the total amount sold.
-// COMBINE THIS WITH THE FUNCTION ABOVE; RETURN TOTAL COOKIES FROM THERE.
-function projectedDailySales(store) {
   var totalCookies = 0;
-  for (var i = 0; i < store.cookiesPerHourArray.length; i++) {
-    totalCookies += store.cookiesPerHourArray[i];
-    console.log('Ongoing total: ' + totalCookies);
+
+  for (var i = 0; i < store.openHoursArray.length; i++) {
+    var cookies = Math.floor(store.custsPerHourArray[i] * store.avgCookiesPerCust);
+    // console.log('Cookies this hour:', cookies);
+    totalCookies += cookies;
+    store.cookiesPerHourArray.push(cookies);
   }
   store.dailySalesTotal = totalCookies;
   return totalCookies;
 }
 
-// Generate and array of the hours the store is open, in a string
-// NOTES FOR TUESDAY: MAKE THIS THE FIRST FUNCTION AND THEN USE THE LENGTH OF THIS ARRAY TO RUN THE FOR LOOP FOR THE LATER CALCULATIONS.
-function createOpenHours(store) {
-  for (var i = 0; i < store.custsPerHourArray.length; i++) {
-    var timeAsString = (i + store.storeOpens) + ':00 - ' + (i + store.storeOpens + 1) + ':00';
-    console.log('Time: ' + timeAsString);
-    store.openHoursArray.push(timeAsString);
-  }
-}
-
 // Display the sales numbers for a given store as a list.
 // CHANGE THE NAME OF THIS FUNCTION TO 'render'
-function displaySalesNumbers(store) {
-  var salesList = document.getElementById(store.storeName);
-  var salesTotal = projectedDailySales(store);
+function render(store) {
+  var salesTotal = projectedSales(store);
+  var salesList = document.getElementById(store.identifier);
 
   for (var i = 0; i < store.cookiesPerHourArray.length; i++) {
     var listItem = document.createElement('li');
@@ -148,15 +146,11 @@ function displaySalesNumbers(store) {
   salesList.appendChild(listItem);
 }
 
-// TUESDAY NOTES: MAYBE NEST FUNCTIONS SO THAT EACH SUCCESSIVE FUNCTION CALLS FROM THE PREVIOUSLY REQUIRED FUNCTION, MAKING CALLING INDIVIDUAL FUNCTIONS HERE UNNECESSARY??? CASCADE THE CALLS WITHIN SUCCESSIVE FUNCTIONS INSTEAD.
 // Generate sales numbers for each store in an array of stores
 function generateSalesNumbers() {
   for (var i = 0; i < allStores.length; i++) {
     var store = allStores[i];
-    generateHourlyTraffic(store);
-    projectedHourlySales(store);
-    createOpenHours(store); //MOVE THIS TO THE TOP AFTER THIS IS UPDATED.
-    displaySalesNumbers(store);
+    render(store);
   }
 }
 
